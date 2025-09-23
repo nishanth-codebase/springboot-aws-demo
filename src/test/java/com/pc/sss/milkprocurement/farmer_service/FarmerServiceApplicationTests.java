1cf0004b18a977d274d983c4f87fd16f1fde9e97
nishanth-codebase <nishanth.juvva@gmail.com>
2025-09-23 22:14:17 +0530

    added testJob

--- src/test/java/com/pc/sss/milkprocurement/farmer_service/FarmerServiceApplicationTests.java
+++ src/test/java/com/pc/sss/milkprocurement/farmer_service/FarmerServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.pc.sss.milkprocurement.farmer_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FarmerServiceApplicationTests {
 
 	@Test
