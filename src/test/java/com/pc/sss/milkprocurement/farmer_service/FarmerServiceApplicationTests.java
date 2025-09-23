8a559647bfe5bcce213b8b916705b586d0066aee
nishanth-codebase <nishanth.juvva@gmail.com>
2025-09-23 22:18:05 +0530

    commented

--- src/test/java/com/pc/sss/milkprocurement/farmer_service/FarmerServiceApplicationTests.java
+++ src/test/java/com/pc/sss/milkprocurement/farmer_service/FarmerServiceApplicationTests.java
@@ -1,15 +1,15 @@
-package com.pc.sss.milkprocurement.farmer_service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
-class FarmerServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.pc.sss.milkprocurement.farmer_service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class FarmerServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
